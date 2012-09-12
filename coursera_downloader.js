1bad767a09093cd056830be780b98107131b6ee3
Markus Seidl <seidl.markus@googlemail.com>
2012-09-12 18:51:32 +0200

    Download all course material, not only the videos (First test).

--- coursera_downloader.js
+++ coursera_downloader.js
@@ -1,6 +1,6 @@
 // insert the "Download All Selected" header and textarea
 
-$("div#page-content > div.item_list").before("<br><br><br><h3>Download All Selected</h3>Cut and paste this bash code into the command line. <a href='http://christiangenco.github.com/Coursera-Video-Downloader-Bookmarklet/#select_all' target='_blank'>How?</a><br><textarea id='cmd' rows='5'></textarea><br><br>")
+$("div#page-content > div.item_list").before("<br><br><br><h3>Download All Selected</h3>Cut and paste this bash code into the command line. <a href='http://ma-ver-ick.github.com/Coursera-Video-Downloader-Bookmarklet/#select_all' target='_blank'>How?</a><br><textarea id='cmd' rows='5'></textarea><br><br>")
 
 // section selectors
 $("h3.list_header").prepend("<a data-placement='left' rel='twipsy' data-original-title='Include chapter in multi-download' style='margin-right:10px'><input type='checkbox' class='multidownload_chapter'></input></a>")
@@ -32,31 +32,53 @@ function refreshSectionCheckmarks(){
 refreshSectionCheckmarks();
 
 // the actual work of writing the command to download the checked videos
+function buildDownloadCommand(sectionIndex, sectionName, videoIndex, baseName, downloadLink, extension) {
+    var cookieHeader = ' --header \"Cookie:' + document.cookie + '\" ';
+
+    var directory = (sectionIndex + 1) + '. ' + sectionName + '/';
+    var filename = directory + (videoIndex + 1) + '. ' + baseName + extension;
+
+    var cmd = 'echo "' + filename + '" && ';
+    cmd += 'mkdir -p "' + directory + '" && ';
+    cmd += 'curl -L -C - ' + cookieHeader + downloadLink + ' -o "' + filename + '"';
+    return cmd;
+}
+
 function buildCommand(){
     var command = "";
     $("h3.list_header").each(function(sectionIndex){
         var sectionName = $(this).text().replace(/Chapter .+ - /,"").replace(/\:/,'-').replace(/^(V|I|X)+\. /,'');
         $(this).parent().next().find("a.lecture-link").each(function(videoIndex){
             var $lectureLink = $(this);
-            var videoName = $.trim($lectureLink.text());
+            var baseName = $.trim($lectureLink.text());
 
             // TODO: here should be a more complex way of finding the links. It should also be possible to download SRT, PDF, ...
             // TODO: what has been seen cannot be unseen!
 
             var links = $lectureLink.parent().find(".item_resource").find("a");
-            var downloadLink = $(links[links.length - 1]).attr('href');
-
-            var cookieHeader = ' --header \"Cookie:'+ document.cookie + '\" ';
-
-            var directory = (sectionIndex+1) + '. ' + sectionName + '/';
-            var filename = directory + (videoIndex+1) + '. ' + videoName + '.mp4';
-
-            var cmd = 'echo "' + filename + '" && ';
-            cmd += 'mkdir -p "' + directory + '" && ';
-            cmd += 'curl -L -C - ' + cookieHeader + downloadLink + ' -o "' + filename + '"';
 
             if($(this).find(".multidownload").attr("checked"))
-                command += cmd + "; ";
+
+                for(var i = 0; i < links.length; i++) {
+                    var $link = $($links[i]);
+                    var downloadLink = link.attr('href');
+
+                    if(downloadLink.contains("txt")) {
+                        extension = "txt";
+                    } else if(downloadLink.contains("srt")) {
+                        extension = "srt";
+                    } else if(downloadLink.contains("pdf")) {
+                        extension = "pdf";
+                    } else if(downloadLink.contains("ppt")) {
+                        extension = "ppt";
+                    } else if(downloadLink.contains("pptx")) {
+                        extension = "pptx";
+                    }
+
+                    //var downloadLink = $(links[links.length - 1]).attr('href');
+                    var cmd = buildDownloadCommand(sectionIndex, sectionName, videoIndex, baseName, downloadLink, extension);
+                    command += cmd + "; ";
+                }
         });
     });
     $("#cmd").val(command);
