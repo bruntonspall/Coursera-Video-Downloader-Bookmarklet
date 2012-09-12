a4c446cf4f7ff51d6e2e9c2addfb2c7beca180c4
Markus Seidl <seidl.markus@googlemail.com>
2012-09-12 16:28:08 +0200

    Lazy merge to master branch.

--- coursera_downloader.js
+++ coursera_downloader.js
@@ -10,50 +10,56 @@ $(".lecture-link").prepend("<a data-placement='left' rel='twipsy' data-original-
 
 // check/uncheck individual videos based on viewed status
 $(".multidownload").each(function(){
-  if($(this).parents("li").hasClass('unviewed'))
-    $(this).prop("checked", true);
+    if($(this).parents("li").hasClass('unviewed'))
+        $(this).prop("checked", true);
 });
 
 
 // check/uncheck section based on its videos
 function refreshSectionCheckmarks(){
-  $(".multidownload_chapter").each(function(){
-    var chapterCheckbox = $(this);
-    chapterCheckbox.prop("checked", true);
+    $(".multidownload_chapter").each(function(){
+        var chapterCheckbox = $(this);
+        chapterCheckbox.prop("checked", true);
 
-    $(this).parents(".list_header_link").next(".item_section_list").find(".multidownload").each(function(){
-      if(!$(this).attr("checked")){
-        chapterCheckbox.prop("checked", false);
-        return;
-      }
+        $(this).parents(".list_header_link").next(".item_section_list").find(".multidownload").each(function(){
+            if(!$(this).attr("checked")){
+                chapterCheckbox.prop("checked", false);
+                return;
+            }
+        });
     });
-  });
 }
 refreshSectionCheckmarks();
 
 // the actual work of writing the command to download the checked videos
 function buildCommand(){
-  var command = "";
-  $("h3.list_header").each(function(sectionIndex){
-    var sectionName = $(this).text().replace(/Chapter .+ - /,"").replace(/\:/,'-').replace(/^(V|I|X)+\. /,'');
-    $(this).parent().next().find("a.lecture-link").each(function(videoIndex){
-      var $lectureLink = $(this);
-      var videoName = $.trim($lectureLink.text());
-      var downloadLink = $lectureLink.attr('href').replace('view','download.mp4');
-      var cookieHeader = ' --header \"Cookie:'+ document.cookie + '\" ';
-      
-      var directory = (sectionIndex+1) + '. ' + sectionName + '/';
-      var filename = directory + (videoIndex+1) + '. ' + videoName + '.mp4';
-      
-      var cmd = 'echo "' + filename + '" && ';
-      cmd += 'mkdir -p "' + directory + '" && ';
-      cmd += 'curl -L -C - ' + cookieHeader + downloadLink + ' -o "' + filename + '"';
+    var command = "";
+    $("h3.list_header").each(function(sectionIndex){
+        var sectionName = $(this).text().replace(/Chapter .+ - /,"").replace(/\:/,'-').replace(/^(V|I|X)+\. /,'');
+        $(this).parent().next().find("a.lecture-link").each(function(videoIndex){
+            var $lectureLink = $(this);
+            var videoName = $.trim($lectureLink.text());
 
-      if($(this).find(".multidownload").attr("checked"))
-        command += cmd + "; ";
+            // TODO: here should be a more complex way of finding the links. It should also be possible to download SRT, PDF, ...
+            // TODO: what has been seen cannot be unseen!
+
+            var links = $lectureLink.parent().find(".item_resource").find("a");
+            var downloadLink = links[links.length - 1].attr('href');
+
+            var cookieHeader = ' --header \"Cookie:'+ document.cookie + '\" ';
+
+            var directory = (sectionIndex+1) + '. ' + sectionName + '/';
+            var filename = directory + (videoIndex+1) + '. ' + videoName + '.mp4';
+
+            var cmd = 'echo "' + filename + '" && ';
+            cmd += 'mkdir -p "' + directory + '" && ';
+            cmd += 'curl -L -C - ' + cookieHeader + downloadLink + ' -o "' + filename + '"';
+
+            if($(this).find(".multidownload").attr("checked"))
+                command += cmd + "; ";
+        });
     });
-  });
-  $("#cmd").val(command);
+    $("#cmd").val(command);
 }
 
 buildCommand();
@@ -61,20 +67,20 @@ buildCommand();
 // clicking a section checkbox
 $(".multidownload_chapter").unbind();
 $(".multidownload_chapter").click(function(e) {
-  var check = $(this).attr("checked") ? true : false;
-  $(this).parents(".list_header_link").next(".item_section_list").find(".multidownload").each(function(){
-    $(this).prop("checked", check);
-    console.log(check);
-  });
+    var check = $(this).attr("checked") ? true : false;
+    $(this).parents(".list_header_link").next(".item_section_list").find(".multidownload").each(function(){
+        $(this).prop("checked", check);
+        console.log(check);
+    });
 
-  buildCommand();  
-  e.stopPropagation();
+    buildCommand();
+    e.stopPropagation();
 });
 
 // clicking a video checkbox
 $(".multidownload").unbind();
 $(".multidownload").click(function(e) {
-  buildCommand();
-  refreshSectionCheckmarks();
-  e.stopPropagation();
+    buildCommand();
+    refreshSectionCheckmarks();
+    e.stopPropagation();
 });
